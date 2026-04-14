fb93946b954bf71a7c7eb537934c15e2b47d61bb
FireIsGood <fireisadequate@gmail.com>
2026-04-14 00:30:49 -0700

    feat: improve about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,11 +2,21 @@
 	<h2>About</h2>
 	<p>
 		This is a point tracking tool for literally just me (FireIsGood). As a user you have no control
-		over your points and can only track the points of users who you know the UUID of. I can change a
-		UUID or username if desired. Changing the UUID of a user will break the tracking of anyone who
-		knew the previous UUID.
+		over your points and can only track the points of users who you know the UUID of.
 	</p>
 	<p>I will not give the admin key to you, if you're wondering.</p>
+	<h2>FAQ</h2>
+	<details>
+		<summary>Can I have my username changed?</summary>
+		Yes, I can change your username with no effect on your UUID or points. Ask me if you would like this
+		done.
+	</details>
+	<details>
+		<summary>Can I have my UUID changed?</summary>
+		Yes, I can change your UUID with no effect on your username or points.
+		<strong>This will break tracking on anyone watching your old UUID.</strong> Ask me if you would like
+		this done.
+	</details>
 	<hr />
 	<p>
 		If you are me (FireIsGood), you can access the secret <a href="/admin">Admin</a> page to authorize
